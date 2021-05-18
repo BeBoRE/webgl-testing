e496256fefe76dc24b1a279e52835aa0d4818b5b
Ayrton-Taede Tromp <ayrton.taede@gmail.com>
2021-05-18 21:28:20 +0200

    Increased the trip

--- shaders/wakFragment.glsl
+++ shaders/wakFragment.glsl
@@ -6,7 +6,7 @@ uniform lowp vec2 u_resolution;
 void main(void) {
   lowp vec2 mouseLocation = u_mouse / u_resolution;
   lowp vec2 uv = gl_FragCoord.xy / u_resolution;
-  lowp float distanceToPixel = distance(u_mouse.xy, gl_FragCoord.xy);
+  lowp float distanceToPixel = distance(u_mouse.xy, gl_FragCoord.xy) * (1.0 - (sin(u_time) + 1.0) / 5.0);
 
   gl_FragColor = vec4(cos(distanceToPixel) * 0.6, sin(distanceToPixel) * 1.0, 0, 1.0);
 }
